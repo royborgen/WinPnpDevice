846e45d93f6c2f340eb3eba42f39a41d5dacccf0
Roy Borgen <60260940+royborgen@users.noreply.github.com>
2022-08-10 21:58:24 +0200

    Update Win_PnpDevice_Disable_Enable.ps1

--- Win_PnpDevice_Disable_Enable.ps1
+++ Win_PnpDevice_Disable_Enable.ps1
@@ -8,11 +8,11 @@ $pnpId = 'HID\VID_044F&PID_0402\7&447A713&0&0000', 'HID\VID_044F&PID_0404\7&378B
 
 #Runs through the device array
 foreach ($deviceId in $pnpId) {
-  #If parameter is enable, enable the device
+  	#If parameter is enable, enable the device
 	if ($action -eq "enable") {
 		Write-Host "Enabling device $deviceId";  Enable-PnpDevice -InstanceId $deviceId -Confirm:$false;
 	}
-  #if parameter is disable, disable the device
+ 	#if parameter is disable, disable the device
 	if ($action -eq "disable") {
 		Write-Host "Disabling device $deviceId";  Disable-PnpDevice -InstanceId $deviceId -Confirm:$false;
 	}
